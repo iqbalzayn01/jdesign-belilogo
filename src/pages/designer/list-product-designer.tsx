a6ce26937d90e7d89be7778f51d85f6da955f6ba
Iqbal Zayyan <73063960+iqbalzayn01@users.noreply.github.com>
2025-03-29 22:50:15 +0700

    update: fix designer page

--- src/pages/designer/list-product-designer.tsx
+++ src/pages/designer/list-product-designer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import {
   Pagination,
@@ -11,19 +11,11 @@ import {
 } from '@/components/ui/pagination';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/ui/button';
+import { usePagination } from '@/hooks/usePagination';
+import { useProducts } from '@/hooks/useProducts';
+import { Product } from '@/types';
 
-// Asumsi: data produk diambil dari state atau API
-const products = Array.from({ length: 29 }, (_, index) => ({
-  id: index,
-  name: `Logo ${index + 1}`,
-  price: 100000,
-}));
-
-// Komponen Kartu Produk
-const ProductCard: React.FC<{ product: (typeof products)[0] }> = ({
-  product,
-}) => {
-  // view count sementara tersimpan di localStorage
+const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   const [localViewCount, setLocalViewCount] = useState(() => {
     const storedViewCount = localStorage.getItem(`viewCount-${product.id}`);
     return storedViewCount ? parseInt(storedViewCount) : 0;
@@ -34,12 +26,19 @@ const ProductCard: React.FC<{ product: (typeof products)[0] }> = ({
     setLocalViewCount(newViewCount);
     localStorage.setItem(`viewCount-${product.id}`, newViewCount.toString());
   };
+
   return (
     <div className="flex flex-col gap-5">
       <Card className="relative py-2.5">
         <CardContent className="px-2.5">
           <div className="aspect-video rounded-xl bg-muted/50" />
         </CardContent>
+        <Link
+          to={'/designer'}
+          className="absolute bottom-0 right-0 z-10 bg-zinc-50 text-[10px] text-zinc-500 border px-1 lg:px-3 py-1 rounded-full"
+        >
+          Lihat Designer
+        </Link>
       </Card>
       <div className="flex w-full justify-between">
         <div className="flex flex-col gap-1">
@@ -48,66 +47,99 @@ const ProductCard: React.FC<{ product: (typeof products)[0] }> = ({
             Rp.{product.price.toLocaleString()}
           </span>
         </div>
-        <Button
-          className="bg-white hover:bg-zinc-100 text-sm text-black border border-zinc-200"
-          asChild
-        >
-          <Link to={'/detaillogo'} onClick={handleBeliLogoClick}>
-            Beli Logo
-          </Link>
-        </Button>
+        {!product.sold && (
+          <Button
+            className="bg-white hover:bg-zinc-100 text-xs lg:text-sm text-black border border-zinc-200"
+            asChild
+          >
+            <Link to={'/detaillogo'} onClick={handleBeliLogoClick}>
+              Beli Logo
+            </Link>
+          </Button>
+        )}
       </div>
     </div>
   );
 };
 
+interface PaginationComponentProps {
+  currentPage: number;
+  totalPages: number;
+  pages: (number | string)[];
+  handlePreviousPage: () => void;
+  handleNextPage: () => void;
+  setCurrentPage: (page: number) => void;
+}
+
+const PaginationComponent: React.FC<PaginationComponentProps> = ({
+  currentPage,
+  totalPages,
+  pages,
+  handlePreviousPage,
+  handleNextPage,
+  setCurrentPage,
+}) => (
+  <Pagination>
+    <PaginationContent>
+      <PaginationItem>
+        <PaginationPrevious
+          aria-disabled={currentPage === 1}
+          onClick={handlePreviousPage}
+          className="cursor-pointer"
+        />
+      </PaginationItem>
+      {pages.map((page, index) => (
+        <PaginationItem key={index}>
+          {page === '...' ? (
+            <PaginationEllipsis />
+          ) : (
+            <PaginationLink
+              href="#"
+              isActive={currentPage === page}
+              onClick={() => typeof page === 'number' && setCurrentPage(page)}
+            >
+              {page}
+            </PaginationLink>
+          )}
+        </PaginationItem>
+      ))}
+      <PaginationItem>
+        <PaginationNext
+          aria-disabled={currentPage === totalPages}
+          onClick={handleNextPage}
+          className="cursor-pointer"
+        />
+      </PaginationItem>
+    </PaginationContent>
+  </Pagination>
+);
+
 export default function ListProductDesigner() {
-  const availableProducts = products.filter((product) => product.id);
+  const { availableProducts, loading, error } = useProducts();
+  const {
+    currentPage,
+    pages,
+    currentItems,
+    handlePreviousPage,
+    handleNextPage,
+    totalPages,
+    setCurrentPage,
+  } = usePagination(availableProducts, {
+    itemsPerPage: 12,
+  });
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 12;
-  const totalPages = Math.ceil(availableProducts.length / productsPerPage);
+  const currentItemsCards = useMemo(() => {
+    return currentItems.map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ));
+  }, [currentItems]);
 
-  const currentProducts = availableProducts.slice(
-    (currentPage - 1) * productsPerPage,
-    currentPage * productsPerPage
-  );
-
-  const getPages = () => {
-    const pages = [];
-    if (totalPages <= 7) {
-      for (let i = 1; i <= totalPages; i++) {
-        pages.push(i);
-      }
-    } else {
-      pages.push(1);
-      if (currentPage <= 4) {
-        for (let i = 2; i <= 5; i++) {
-          pages.push(i);
-        }
-        pages.push('...');
-        pages.push(totalPages);
-      } else if (currentPage >= totalPages - 3) {
-        pages.push('...');
-        for (let i = totalPages - 4; i <= totalPages - 1; i++) {
-          pages.push(i);
-        }
-        pages.push(totalPages);
-      } else {
-        pages.push('...');
-        for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-          pages.push(i);
-        }
-        pages.push('...');
-        pages.push(totalPages);
-      }
-    }
-    return pages;
-  };
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <section className="bg-zinc-100">
-      <div className="container mx-auto flex flex-col gap-10 px-5 py-20">
+      <div className="container mx-auto flex flex-col gap-10 px-5 py-24">
         <div className="grid gap-10">
           <div>
             <h2 className="text-[38px]">Logo dibuat oleh Designer</h2>
@@ -118,46 +150,17 @@ export default function ListProductDesigner() {
             </p>
           </div>
           <div className="grid md:grid-cols-3 lg:grid-cols-4 md:gap-5 lg:gap-10">
-            {currentProducts.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+            {currentItemsCards}
           </div>
         </div>
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                aria-disabled={currentPage === 1}
-                onClick={() => setCurrentPage(currentPage - 1)}
-                className="cursor-pointer"
-              />
-            </PaginationItem>
-            {getPages().map((page, index) => (
-              <PaginationItem key={index}>
-                {page === '...' ? (
-                  <PaginationEllipsis />
-                ) : (
-                  <PaginationLink
-                    href="#"
-                    isActive={currentPage === page}
-                    onClick={() =>
-                      typeof page === 'number' && setCurrentPage(page)
-                    }
-                  >
-                    {page}
-                  </PaginationLink>
-                )}
-              </PaginationItem>
-            ))}
-            <PaginationItem>
-              <PaginationNext
-                aria-disabled={currentPage === totalPages}
-                onClick={() => setCurrentPage(currentPage + 1)}
-                className="cursor-pointer"
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+        <PaginationComponent
+          currentPage={currentPage}
+          totalPages={totalPages}
+          pages={pages}
+          handlePreviousPage={handlePreviousPage}
+          handleNextPage={handleNextPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </section>
   );
